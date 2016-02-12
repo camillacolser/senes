1d65e93dc95870dc6837f3437a76f812a7c3486c
emmabeynon <emma.beynon@gmail.com>
2016-02-12 14:53:18 +0000

    Added tests for displaying the 'today' and 'week' tabs

--- mobile/test/e2e/appFeature.js
+++ mobile/test/e2e/appFeature.js
@@ -1,13 +1,15 @@
 describe('Senior Ionic', function() {
-  var today = element(by.css('a.active'));
-  it('has a title', function() {
-    browser.get('http://localhost:8100');
-    expect(browser.getTitle()).toEqual('Senior Ionic');
-  });
+  var today = element(by.css('h1'));
+  var week = element(by.linkText('Week'));
 
   it('displays the \'today \' tab by default', function() {
     browser.get('http://localhost:8100');
-    expect(today.getText()).toEqual('Today');
+    expect(browser.getTitle()).toEqual('Today');
   });
 
+  it('displays the \'week\' tab when clicked on', function() {
+    browser.get('http://localhost:8100');
+    week.click();
+    expect(browser.getTitle()).toEqual('Week');
+  });
 });

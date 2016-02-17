c2490e4ea7288062ab87ee367ab4897196a67af6
emmabeynon <emma.beynon@gmail.com>
2016-02-17 10:49:49 +0000

    Minor change to heart_rate_on_date method

--- web/app/controllers/fitbit_api_controller.rb
+++ web/app/controllers/fitbit_api_controller.rb
@@ -5,7 +5,7 @@ class FitbitApiController < ApplicationController
     devise_id = params[:id]
     @user = User.find_by(id: devise_id)
     client = @user.fitbit_client
-    heart_parsed = client.heart_rate_on_date('today')['activities-heart'][0]['value']['restingHeartRate']
+    heart_parsed = client.heart_rate_on_date('1d')['activities-heart'][0]['value']['restingHeartRate']
     sleep_parsed = client.sleep_logs_on_date('today')['summary']['totalMinutesAsleep']
     steps_parsed = client.steps_on_date('today')['activities-steps'][0]['value']
     @json = {

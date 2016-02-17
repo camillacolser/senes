9f58c4908015b78f3ca534e9c93c97c8e9d84e09
allimac <camillacolser@hotmail.com>
2016-02-17 16:34:48 +0000

    initial work on update alarms

--- web/app/controllers/fitbit_api_controller.rb
+++ web/app/controllers/fitbit_api_controller.rb
@@ -57,14 +57,14 @@ class FitbitApiController < ApplicationController
     render json: { 'trackerAlarms': response }
   end
 
-  # def update_alarm
-  #   devise_id = params[:id]
-  #   time = params[:time]
-  #   client = User.find_by(id: devise_id).fitbit_client
-  #   tracker_id = client.device_info[0]['id']
-  #   response = client.post_alarm(tracker_id, time)
-  #   render json: { 'response': response }
-  # end
+  def update_alarm
+    devise_id = params[:id]
+    time = params[:time]
+    client = User.find_by(id: devise_id).fitbit_client
+    tracker_id = client.device_info[0]['id']
+    response = client.post_alarm(tracker_id, time)
+    render json: { 'response': response }
+  end
 
   def delete_alarm
     devise_id = params[:id]
